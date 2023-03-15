54500fa83628b0a820d7bb51156f1c7d03569e73
TripAndCode <devyolouis@gmail.com>
2023-03-15 22:33:34 +0900

    add GA4 tracking id

--- app/head.tsx
+++ app/head.tsx
@@ -1,11 +1,24 @@
 // HTTP HEAD ELEMENT (METADATA FOR PAGES)
 
 export default function Head() {
+
   return (
     <>
       <title>Trip&Code</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon-32x32.png" />
+      {/* <!-- Google tag (gtag.js) --> */}
+      <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA4_TRACKING_ID}`}></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GA4_TRACKING_ID}');
+          `,
+        }}
+      />
     </>
   )
 }
