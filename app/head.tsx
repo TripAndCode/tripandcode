56dd8faf33676277c7e9b15781c488fd6cb7e0f2
TripAndCode <devyolouis@gmail.com>
2023-03-15 22:48:15 +0900

    retry ga

--- app/head.tsx
+++ app/head.tsx
@@ -1,4 +1,5 @@
 // HTTP HEAD ELEMENT (METADATA FOR PAGES)
+import Script from 'next/script'
 
 export default function Head() {
 
@@ -9,18 +10,20 @@ export default function Head() {
       <meta content="width=device-width, initial-scale=1" name="viewport" />
       <link rel="icon" href="/favicon-32x32.png" />
       {/* <!-- Google tag (gtag.js) --> */}
-      {/* <script async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA4_TRACKING_ID}`}></script>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+      <Script
+        async src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA4_TRACKING_ID}`}
+        strategy="afterInteractive"
+      ></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+         window.dataLayer = window.dataLayer || [];
+         function gtag(){dataLayer.push(arguments);}
+         gtag('js', new Date());
 
-            gtag('config', '${process.env.NEXT_PUBLIC_GA4_TRACKING_ID}');
-          `,
-        }}
-      /> */}
+         gtag('config', '${process.env.NEXT_PUBLIC_GA4_TRACKING_ID}');
+        `}
+
+      </Script>
     </>
   )
 }
